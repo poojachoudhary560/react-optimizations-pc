2e426cf430d503992dde117b9d3c474f4f4341a4
poojachoudhary560 <poojachoudhary560@gmail.com>
2021-05-17 04:12:15 +0530

    1 file modified

--- src/BasicErrorBoundaries/Parent.js
+++ src/BasicErrorBoundaries/Parent.js
@@ -3,11 +3,14 @@ import BuggyCounter from './BuggyCounter';
 import MyErrorBoundary from './MyErrorBoundary';
 
 const Parent = () => {
+  refreshPage = () => history.go(0);
   return (
     <>
       <MyErrorBoundary>
         <BuggyCounter />
       </MyErrorBoundary>
+      <hr />
+      <button onClick={this.refreshPage}>Refresh Page</button>
     </>
   );
 };
