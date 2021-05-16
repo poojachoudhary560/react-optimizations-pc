2f96fde4bd4f8790d335814984d2ad6459e2a356
poojachoudhary560 <poojachoudhary560@gmail.com>
2021-05-17 04:16:56 +0530

    1 file modified

---
+++ src/BasicPureComponent/DefaultComp.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Child from './Child';
+
+class DefaultComp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 1
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => setState({ value: 1 }), 3000);
+  }
+
+  render() {
+    console.log('render default Comp');
+    return <Child value={this.state.value} />;
+  }
+}
+
+export default DefaultComp;
