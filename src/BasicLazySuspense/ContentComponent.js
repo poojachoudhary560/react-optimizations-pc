25c9461b9d43993fdfce1ee09e161e242e4c005e
poojachoudhary560 <poojachoudhary560@gmail.com>
2021-05-17 02:24:49 +0530

    3 files added

---
+++ src/BasicLazySuspense/ContentComponent.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ContentComponent = () => {
+  return (
+    <div>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry...
+      </p>
+      <p>
+        It is a long established fact that a reader will be distracted by the
+        readable content of a page when looking at its layout...
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua...
+      </p>
+    </div>
+  );
+};
+
+export default ContentComponent;
---
+++ src/BasicLazySuspense/ImageComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ImageComponent = () => {
+  return (
+    <img
+      src="https://images.unsplash.com/photo-1517694712202-14dd9538aa97"
+      width="960"
+      height="480"
+      alt="coding"
+    />
+  );
+};
+
+export default ImageComponent;
