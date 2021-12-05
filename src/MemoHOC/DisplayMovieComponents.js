a7804ba01e0976a31426a430abc2d0805f90743c
poojachoudhary560 <poojachoudhary560@gmail.com>
2021-12-05 16:36:06 +0530

    3 files added

---
+++ src/MemoHOC/DisplayMovieComponents.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MovieMemo from './movieMemo';
+
+export default DisplayCaptureSurfaceType = (props) => {
+  return (
+    <div>
+      <div>
+        <p> Basic Movie Compoenent with React.memo() </p>
+        <MovieMemo />
+      </div>
+    </div>
+  );
+};
---
+++ src/MemoHOC/MovieMemo.js
@@ -0,0 +1,25 @@
+// React.memo() is a HOC
+// React.memo() wraps a component
+// when React.memo() wraps a component, React memoizes the
+// rendered output of wrapped component and skips unnecessary
+// rendering
+//
+// When deciding to update DOM, React first renders your
+// component, then compares the result with the previous render.
+// If the render results are different, React updates the DOM
+
+// When a component is wrapped in react.memo(), React renders
+// the component and memoizes the result. Before the next
+// render, if the props are same, React uses the memoized result
+// skipping the next rendering.
+
+export function MovieMemo({ title, releaseDate }) {
+  return (
+    <div>
+      <div>Movie Title: {title}</div>
+      <div>Release date: {releaseDate} </div>
+    </div>
+  );
+}
+
+export const MemoizedMovie = React.memo(MovieMemo);
